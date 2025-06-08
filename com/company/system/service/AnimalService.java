ca06cf47495c41ab06668e3caa7cb32e07c810c9
GabrielNat1 <dcbiel846@gmail.com>
2025-06-08 16:17:03 -0300

    feat: add method isOlderThan in AnimalService to compare animal ages

--- com/company/system/service/AnimalService.java
+++ com/company/system/service/AnimalService.java
@@ -1,4 +1,9 @@
 package com.company.system.service;
 
+import com.company.system.model.Animal;
+
 public class AnimalService {
+    public boolean isOlderThan(Animal animal1, Animal animal2){
+        return animal1.getYear() < animal2.getYear();
+    }
 }
