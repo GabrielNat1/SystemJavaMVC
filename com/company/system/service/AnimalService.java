db33d8756192dca0e0f99631b8377398a5c01deb
GabrielNat1 <dcbiel846@gmail.com>
2025-06-08 16:49:20 -0300

    feat: add list of animals in AnimalService

--- com/company/system/service/AnimalService.java
+++ com/company/system/service/AnimalService.java
@@ -2,7 +2,16 @@ package com.company.system.service;
 
 import com.company.system.model.Animal;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AnimalService {
+    private List<Animal> animals;
+
+    public animalService(){
+        animals = new ArrayList<>();
+    }
+
     public boolean isOlderThan(Animal animal1, Animal animal2){
         return animal1.getYear() < animal2.getYear();
     }
